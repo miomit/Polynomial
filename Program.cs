32efe0bd1ab8b8d3b15464f83d8b52a240574824
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-11-26 21:17:44 +0400

    new test

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿Polynomial p = new("-1 0 0 1");
-Polynomial p2 = new("-1 0 1");
+﻿Polynomial p = new("-3 -4 -1 3 1");
+Polynomial p2 = new("-3 2 10 3");
 
 Console.WriteLine($"p \t= {p}\np2 \t= {p2}\n");
 
-
 Console.WriteLine(p / p2);
 Console.WriteLine(p % p2);
+
 Console.WriteLine(Polynomial.NOD(p, p2));
